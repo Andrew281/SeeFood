2d07a3184e242641fa25f28462f0dfc0be3c6770
Андрей Чеканников <andrew281@rambler.ru>
2019-04-24 16:20:12 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Андрей Чеканников on 24/04/2019.
+//  Copyright © 2019 Andrey Chekannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
